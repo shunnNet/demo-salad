cb66a827fc9f077ad7f5f5f7b984e1a5f7c255ab
net <wendell20904102@gmail.com>
2023-04-16 15:03:46 +0800

    basic env

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@/scss/index.scss'
-import { loadFont } from '@/utils/browser.js'
-
-loadFont('https://use.typekit.net/wel8ibk.css')
 
 createApp(App).mount('#app')
--- src/store/breakpoints.js
+++ src/store/breakpoints.js
@@ -1,15 +1,19 @@
 import { useBreakpoints } from '@vueuse/core'
 
 export const breakpoints = useBreakpoints({
+  desktopWide: 1920,
   desktop: 1440,
   laptop: 1024,
   tablet: 768,
   mobile: 576,
+  mobileMini: 375,
 })
 
 export const {
+  desktopWide: overDesktopWide,
   desktop: overDesktop,
   laptop: overLaptop,
   tablet: overTablet,
   mobile: overMobile,
+  mobileMini: overMobileMini,
 } = breakpoints
